b7c38dc4b529d09ed79e8dc9df32b60601b5ecbb
morhook <cruz@scvsoft.com>
2024-10-16 12:45:58 -0300

    Agregada ambientación de pajaritos para que la gente se relaje mas. Usamos ffmpeg directo porque con fluent no andaba el mixer por algunar razón

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import OpenAI from 'openai';
 import fs from 'fs';
 import path from 'path';
+import { exec }  from  'child_process';
 
 const openai = new OpenAI(); // process.env.OPENAI_API_KEY by default apiKey
 
+console.log('generating meditation');
 const response = await openai.chat.completions.create({
   messages: [
     {
       role: 'system',
-      content: 'Eres un asistente que genera meditaciones guiadas. Cada dos o tres oraciones agregar "[very-long-pause][very-long-pause]" para generar pausas'
+      content: 'Eres un asistente que genera meditaciones guiadas. Cada dos o tres oraciones agregar "[very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause][very-long-pause]" para generar pausas'
     },
     {
       role: 'user', content: 'Haz una meditación para terminar el día de 20 minutos y pensar sobre los fracasos y triunfos. necesito ejercicios de respiración y visualización de las energias.'
@@ -21,7 +23,7 @@ const response = await openai.chat.completions.create({
 
 console.log(response)
 console.log(response.choices[0].message.content.trim());
-
+console.log('generating speech')
 const mp32 = await openai.audio.speech.create({
   input: response.choices[0].message.content.trim(),
   model: 'tts-1', // 'tts-1' | 'tts-1-hd'
@@ -31,3 +33,17 @@ const mp32 = await openai.audio.speech.create({
 });
 const buffer2 = Buffer.from(await mp32.arrayBuffer());
 await fs.promises.writeFile(path.resolve('/tmp/speech2.mp3'), buffer2);
+console.log('generated initial MP3')
+console.log('mixing with birds and sounds')
+
+let command = "ffmpeg -y -i /tmp/speech2.mp3 -i ambient-forest-sounds.m4a -filter_complex amix=inputs=2:duration=longest /tmp/mixed_audio.mp3"
+var yourscript = exec(command, 
+  (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (error !== null) {
+        console.log(`exec error: ${error}`);
+    }
+});
+
+console.log('meditation done!')
