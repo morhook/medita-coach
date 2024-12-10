dd3cac86ab04e7cd8c4c28fde99716c0e16b5b2d
morhook <cruz@scvsoft.com>
2024-12-10 12:50:12 -0300

    Agregada API telegram. Usamos ngrok al final.
    Generamos la meditacion cuando el usuario dice meditar, todavia no la mandamos. Con el comando ping respondemos pong para mostrar que estamos trabajando.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import OpenAI from 'openai';
 import fs from 'fs';
 import path from 'path';
 import ffmpeg from 'fluent-ffmpeg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const openai = new OpenAI(); // process.env.OPENAI_API_KEY by default apiKey
 
